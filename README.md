c2fc312eaebd79f99ef8c0743dc6486862695ddf
Tom <tom@m0lte.uk>
2023-08-18 21:38:33 +0100

    Publish kissproxy

---
+++ README.md
@@ -0,0 +1,143 @@
+# kissproxy
+
+Serial-to-TCP proxy for serial KISS modems, including MQTT tracing support. That is, a program which connects to a serial KISS modem, makes it available on a TCP port so it can continue to be used as normal by node software, and also outputs all the traffic between the node software and the modem to an MQTT host of your choosing, with various options.
+
+## Usage
+
+```
+$ ./kissproxy
+Description:
+  
+Usage:
+  kissproxy [options]
+
+Options:
+  -c, --comport <comport> (REQUIRED)  The COM port the modem is connected to, e.g. /dev/ttyACM0
+  -b, --baud <baud>                   The baud rate of the modem [default: 57600]
+  -p, --tcpport <tcpport>             The TCP port to listen on [default: 8910]
+  -a, --anyhost                       Whether to accept connections from any host, instead of just localhost [default: False]
+  -m, --mqtt-server <mqtt-server>     MQTT server to forward KISS frames to
+  -mu, --mqtt-user <mqtt-user>        MQTT username
+  -mp, --mqtt-pass <mqtt-pass>        MQTT password
+  -mt, --mqtt-topic <mqtt-topic>      MQTT topic
+  --base64                            Publish base64 strings rather than raw bytes
+  --version                           Show version information
+  -?, -h, --help                      Show help and usage information
+```
+
+## Topics
+
+This program outputs by default to a topics named as follows:
+
+```
+kissproxy/$hostname/$comport/$direction
+```
+
+`$direction` can be `toModem` or `fromModem`.
+
+Under that topic are further sub-topics:
+
+`/framed` - the payload is the raw traffic between the node and the modem, with its KISS framing intact.
+
+`/unframed/$kissport/$kisscommand` - for these topics, the MQTT payload is the unpacked/unescaped frame, normally but not necessarily AX.25, separated by TNC port and KISS command id.
+
+The KISS port will always be port0 for single-port TNCs.
+
+The KISS command represents the command byte unpacked from the KISS framing.
+
+## Windows usage
+
+Should just be a case of 
+
+```
+git clone https://github.com/M0LTE/kissproxy.git
+cd kissproxy\src\kissproxy
+dotnet run
+```
+
+assuming .NET 7 is installed. Then point LinBPQ at it as below. Awaiting feedback / demonstrated need, since it's not too common to run a packet node on Windows these days, let alone debug one.
+
+## Linux Build / Install
+
+### Prerequisites
+.NET 7 SDK:
+```
+curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --channel STS
+echo 'export DOTNET_ROOT=$HOME/.dotnet' >> ~/.bashrc
+echo 'export PATH=$PATH:$HOME/.dotnet' >> ~/.bashrc
+source ~/.bashrc
+dotnet --version
+```
+
+### Build
+
+Tested on 32 bit Raspberry Pi OS Lite v11 on Raspberry Pi 2B rev 1.1.
+
+```
+git clone https://github.com/M0LTE/kissproxy.git
+cd kissproxy
+dotnet publish src/kissproxy/kissproxy.csproj --configuration Release -p:PublishProfile=src/kissproxy/Properties/PublishProfiles/Linux-arm32.pubxml
+```
+
+### Install as systemd service
+
+First stop LinBPQ to free up the modem port.
+
+Note the modem COM port and MQTT server specified below - adjust as required before running.
+
+```
+sudo mkdir /opt/kissproxy
+sudo mv publish/* /opt/kissproxy/
+sudo sh -c 'echo "[Unit]
+After=network.target
+[Service]
+ExecStart=/opt/kissproxy/kissproxy --comport /dev/ttyACM0 --mqtt-server myhost
+WorkingDirectory=/opt/kissproxy
+Restart=always
+[Install]
+WantedBy=multi-user.target" > /etc/systemd/system/kissproxy.service'
+sudo systemctl enable kissproxy
+sudo systemctl start kissproxy
+```
+
+### Configure linbpq (example)
+
+In your `bpq32.cfg`, find your KISS port of interest.
+
+Change from like this:
+
+```
+PORT
+  PORTNUM=1
+  ID=VHF
+  TYPE=ASYNC
+  PROTOCOL=KISS
+  COMPORT=/dev/serial/by-path/platform-3f980000.usb-usb-0:1.2:1.0
+  SPEED=57600
+  ...
+ENDPORT
+```
+
+to this:
+
+```
+PORT
+  PORTNUM=1
+  ID=VHF
+  TYPE=ASYNC
+  PROTOCOL=KISS
+  ;COMPORT=/dev/serial/by-path/platform-3f980000.usb-usb-0:1.2:1.0
+  ;SPEED=57600
+  IPADDR=127.0.0.1
+  TCPPORT=8910
+  ...
+ENDPORT
+```
+
+This causes BPQ to connect to the modem via this program, rather than directly.
+
+Start up LinBPQ and check syslog to ensure kissproxy reports it has received a node connection, has connected to the modem, and has connected to your MQTT server.
+
+## Licence
+
+MIT. Fill your boots.
---
+++ src/kissproxy/Program.cs
@@ -0,0 +1,297 @@
+﻿using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Extensions.ManagedClient;
+using MQTTnet.Protocol;
+using System.CommandLine;
+using System.IO.Ports;
+using System.Net;
+using System.Net.Sockets;
+using static kissproxy.KissHelpers;
+
+var comPortOption = new Option<string?>("--comport", "The COM port the modem is connected to, e.g. /dev/ttyACM0")
+{
+    IsRequired = true,
+};
+comPortOption.AddAlias("-c");
+
+var baudOption = new Option<int>("--baud", "The baud rate of the modem");
+baudOption.AddAlias("-b");
+baudOption.SetDefaultValue(57600);
+
+var tcpPortOption = new Option<int>("--tcpport", "The TCP port to listen on");
+tcpPortOption.AddAlias("-p");
+tcpPortOption.SetDefaultValue(8910);
+
+var anyHostOption = new Option<bool>(
+    name: "--anyhost",
+    description: "Whether to accept connections from any host, instead of just localhost",
+    getDefaultValue: () => false);
+anyHostOption.AddAlias("-a");
+
+var brokerOption = new Option<string?>("--mqtt-server", "MQTT server to forward KISS frames to");
+brokerOption.AddAlias("-m");
+
+var brokerUserOption = new Option<string?>("--mqtt-user", "MQTT username");
+brokerUserOption.AddAlias("-mu");
+
+var brokerPasswordOption = new Option<string?>("--mqtt-pass", "MQTT password");
+brokerPasswordOption.AddAlias("-mp");
+
+var brokerTopicOption = new Option<string?>("--mqtt-topic", "MQTT topic");
+brokerTopicOption.AddAlias("-mt");
+
+var publishBase64Option = new Option<bool>("--base64", "Publish base64 strings rather than raw bytes");
+
+var rootCommand = new RootCommand("Serial-to-TCP proxy for serial KISS modems, including MQTT tracing support.");
+rootCommand.AddOption(comPortOption);
+rootCommand.AddOption(baudOption);
+rootCommand.AddOption(tcpPortOption);
+rootCommand.AddOption(anyHostOption);
+rootCommand.AddOption(brokerOption);
+rootCommand.AddOption(brokerUserOption);
+rootCommand.AddOption(brokerPasswordOption);
+rootCommand.AddOption(brokerTopicOption);
+rootCommand.AddOption(publishBase64Option);
+
+/*rootCommand.SetHandler(
+    (comport, baud, tcpPort, anyHost, mqttServer, mqttUser, mqttPassword, mqttTopic, base64) 
+        => Run(comport!, baud, tcpPort, anyHost, mqttServer, mqttUser, mqttPassword, mqttTopic, base64),
+    comPortOption,
+    baudOption,
+    tcpPortOption,
+    anyHostOption,
+    brokerOption, 
+    brokerUserOption,
+    brokerPasswordOption,
+    brokerTopicOption,
+    publishBase64Option);*/
+
+rootCommand.SetHandler(async context => { 
+
+    var comPort = context.ParseResult.GetValueForOption(comPortOption);
+    var baud = context.ParseResult.GetValueForOption(baudOption);
+    var tcpPort = context.ParseResult.GetValueForOption(tcpPortOption);
+    var anyHost = context.ParseResult.GetValueForOption(anyHostOption);
+    var mqttServer = context.ParseResult.GetValueForOption(brokerOption);
+    var mqttUser = context.ParseResult.GetValueForOption(brokerUserOption);
+    var mqttPassword = context.ParseResult.GetValueForOption(brokerPasswordOption);
+    var mqttTopic = context.ParseResult.GetValueForOption(brokerTopicOption);
+    var base64 = context.ParseResult.GetValueForOption(publishBase64Option);
+
+    using var sp = new SerialPort(comPort!, baud);
+    try
+    {
+        sp.Open();
+    }
+    catch (Exception ex)
+    {
+        Exit($"Could not connect to {comPort}: {ex.Message}, terminating");
+        return;
+    }
+
+    LogInformation($"Connected to modem on {comPort} at {baud}");
+
+    Action<List<byte>>? tcpSend = null, serialSend = buffer => sp.Write(buffer.ToArray(), 0, buffer.Count);
+    IManagedMqttClient? mqttClient = null;
+
+    _ = Task.Run(async () =>
+    {
+        try
+        {
+            List<byte> serialBuffer = new();
+            while (true)
+            {
+                int read;
+                try
+                {
+                    read = sp.ReadByte();
+                }
+                catch (OperationCanceledException)
+                {
+                    Exit("Modem has disconnected, terminating");
+                    return;
+                }
+
+                if (read == -1)
+                {
+                    Exit("Modem returned -1, terminating");
+                    return;
+                }
+
+                serialBuffer.Add((byte)read);
+                await ProcessBuffer(serialBuffer, tcpSend, false, mqttClient, comPort!, mqttTopic, base64);
+            }
+        }
+        catch (Exception ex)
+        {
+            Exit($"Exception: {ex.Message}, terminating");
+            return;
+        }
+    });
+
+    _ = Task.Run(async () =>
+    {
+        try
+        {
+            while (true)
+            {
+                TcpListener tcpListener = new(anyHost ? IPAddress.Any : IPAddress.Loopback, tcpPort);
+                tcpListener.Start();
+                LogInformation($"Awaiting node connection on port {tcpPort}");
+
+                using var tcpClient = tcpListener.AcceptTcpClient();
+                using var tcpStream = tcpClient.GetStream();
+                tcpSend = buffer => tcpStream.Write(buffer.ToArray(), 0, buffer.Count);
+                LogInformation("Accepted TCP node connection");
+
+                List<byte> tcpBuffer = new();
+                while (true)
+                {
+                    var read = tcpStream.ReadByte();
+                    if (read == -1)
+                    {
+                        LogInformation("Node disconnected");
+                        tcpSend = null;
+                        break;
+                    }
+                    tcpBuffer.Add((byte)read);
+                    await ProcessBuffer(tcpBuffer, serialSend, true, mqttClient, comPort!, mqttTopic, base64);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Exit($"Exception: {ex.Message}, terminating");
+            return;
+        }
+    });
+
+    if (!string.IsNullOrWhiteSpace(mqttServer))
+    {
+        var (server, port) = SplitMqttServer(mqttServer);
+
+        LogInformation($"Connecting to MQTT broker {server}:{port}...");
+
+        var options = new ManagedMqttClientOptionsBuilder()
+            .WithAutoReconnectDelay(TimeSpan.FromSeconds(5))
+            .WithClientOptions(new MqttClientOptionsBuilder()
+                .WithClientId("kissproxy")
+                .WithTcpServer(server, port)
+                .WithCredentials(mqttUser, mqttPassword)
+                //.WithTls() //TODO
+                .Build())
+            .Build();
+
+        mqttClient = new MqttFactory().CreateManagedMqttClient();
+        //await mqttClient.SubscribeAsync(new MqttTopicFilterBuilder().WithTopic("my/topic").Build());
+        await mqttClient.StartAsync(options);
+        mqttClient.ConnectedAsync += MqttClient_ConnectedAsync;
+        mqttClient.ConnectingFailedAsync += MqttClient_ConnectingFailedAsync;
+        mqttClient.DisconnectedAsync += MqttClient_DisconnectedAsync;
+    }
+
+    Thread.CurrentThread.Join();
+});
+
+return rootCommand.Invoke(args);
+
+static Task MqttClient_DisconnectedAsync(MqttClientDisconnectedEventArgs arg)
+{
+    LogInformation("Disconnected from MQTT broker: " + arg?.ReasonString);
+    return Task.CompletedTask;
+}
+
+static Task MqttClient_ConnectingFailedAsync(ConnectingFailedEventArgs arg)
+{
+    LogInformation("Failed to connect to MQTT broker: " + arg?.Exception?.Message);
+    return Task.CompletedTask;
+}
+
+static Task MqttClient_ConnectedAsync(MqttClientConnectedEventArgs arg)
+{
+    LogInformation("Connected to MQTT broker");
+    return Task.CompletedTask;
+}
+
+static (string server, int port) SplitMqttServer(string mqttServer)
+{
+    var parts = mqttServer.Split(':');
+    if (parts.Length ==2 && int.TryParse(parts[1], out var port))
+    {
+        return (parts[0], port);
+    }
+    return (parts[0], 1883);
+}
+
+static async Task ProcessBuffer(List<byte> buffer, Action<List<byte>>? send, bool toModem, IManagedMqttClient? client, string comPort, string? topic, bool convertToBase64)
+{
+    DiscardRepeatedFends(buffer);
+
+    if (IsKissFrame(buffer))
+    {
+        await PublishKissFrame(client, buffer, toModem, comPort, topic, convertToBase64);
+
+        if (send == null)
+        {
+            LogInformation("Nowhere to send a frame, discarded.");
+        }
+        else
+        {
+            send(buffer);
+            //LogInformation($"{(toModem ? ">" : "<")} {buffer.Count}b");
+        }
+
+        buffer.Clear();
+    }
+}
+
+static async Task PublishKissFrame(IManagedMqttClient? client, List<byte> buffer, bool toModem, string comPort, string? topic, bool convertToBase64)
+{
+    if (client == null)
+    {
+        return;
+    }
+
+    topic ??= $"kissproxy/{Sanitise(Environment.MachineName)}/{Sanitise(comPort)}/{(toModem ? "to" : "from")}Modem";
+
+    await EnqueueBytes(client, $"{topic}/framed", buffer, convertToBase64);
+    try
+    {
+        var (rawFrame, portId, commandCode) = Unkiss(buffer);
+        await EnqueueBytes(client, $"{topic}/unframed/port{portId}/{commandCode}KissCmd", rawFrame, convertToBase64);
+    }
+    catch (Exception ex)
+    {
+        LogInformation($"Problem unframing KISS frame: {ex.Message}");
+    }
+}
+
+static async Task EnqueueBytes(IManagedMqttClient client, string topic, IList<byte> bytes, bool convertToBase64)
+{
+    var messageBuilder = new MqttApplicationMessageBuilder()
+        .WithTopic(topic)
+        .WithQualityOfServiceLevel(MqttQualityOfServiceLevel.ExactlyOnce);
+
+    messageBuilder = convertToBase64
+        ? messageBuilder.WithPayload(Convert.ToBase64String(bytes.ToArray(), Base64FormattingOptions.InsertLineBreaks))
+        : messageBuilder.WithPayload(bytes.ToArray());
+
+    await client.EnqueueAsync(messageBuilder.Build());
+}
+
+static string Sanitise(string comPort)
+{
+    var parts = comPort.Split('/');
+    return parts.Last();
+}
+
+static void LogInformation(string message)
+{
+    Console.WriteLine($"{DateTime.UtcNow:HH:mm:ss.ff}Z  {message}");
+}
+
+static void Exit(string v)
+{
+    LogInformation(v);
+    Environment.Exit(1);
+}
